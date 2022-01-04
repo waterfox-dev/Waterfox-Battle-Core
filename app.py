44821ae4bd77d13baf527f4baf0face400303484
Waterfox <waterfox80@gmail.com>
2022-01-04 18:40:09 +0100

    Creator finished !!!!

--- app.py
+++ app.py
@@ -53,17 +53,50 @@ def upload_to_server(localpath : str, name : str) :
     ssh.close()
 
 def create_opponent(data_dict : dict, name : str):
+    data_dict = serialize(data_dict)
     file = open(f'static/data/opponent/{name}.json', 'x')
+    
     n_object = {}
     n_object['name'] = data_dict['nameInput']
     n_object['type'] = "opponent"
-    n_object['life'] = data_dict['lifeInput']
-    n_object['defense'] = data_dict['defenseInput']
-    n_object['maxLife'] = data_dict['lifeInput']
+    n_object['life'] = int(data_dict['lifeInput'])
+    n_object['defense'] = int(data_dict['defenseInput'])
+    n_object['maxLife'] = int(data_dict['lifeInput'])    
+    n_object['attacks'] = data_dict['attacks']
     n_object['theme'] = data_dict['themeInput'][:-4]
+    n_object['sprite'] = f"../static/images/{data_dict['fileName']}.png"
+    
     json.dump(n_object, file)
     file.close()
-        
+
+    with open('static/data/register.json', 'r', encoding='utf8') as r_file :
+        print(data_dict)
+        r_file = json.load(r_file)
+        r_file[data_dict['publicCode']] = {
+            'opponent' : f"../static/data/opponent/{data_dict['themeInput']}.json",
+            "player" : "../static/data/player/testPlayer.json", 
+            "name" : data_dict['nameInput']
+        }
+
+        with open('static/data/register.json', 'w', encoding='utf8') as w_file :
+            json.dump(r_file, w_file)
+    
+
+def serialize(data_dict : dict):
+    data_dict['attacks'] = []
+    current = 0
+    for key in data_dict.keys() :
+        if 'attackName' in key :
+            current += 1
+            data_dict['attacks'].append(
+                {"name" : data_dict[f"attackName{current}"], 
+                "description" : data_dict[f"attackDescription{current}"],
+                "attack" : int(data_dict[f"attackPoint{current}"]), 
+                "precision" : int(data_dict[f"precisionPoint{current}"])
+                }
+            )
+    return data_dict
+            
 
 
 #Router
